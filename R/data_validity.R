1ea0c3580fc7faf7b370d0dc62661487b40c8098
Russ Hyde <russHyde@users.noreply.github.com>
2018-12-05 19:07:56 +0000

    fix #1: new function `feature_counts_to_dgelist` (#3)
    new function `feature_counts_to_dgelist`

--- R/data_validity.R
+++ R/data_validity.R
@@ -1,3 +1,48 @@
-.is_single_string <- function(.x) {
+###############################################################################
+
+#' is_single_string
+#'
+#' @noRd
+
+is_single_string <- function(.x) {
   is.character(.x) && length(.x) == 1
+}
+
+###############################################################################
+
+#' is_nonempty_df - checks if an R object is a data-frame and has at least one
+#' column and at least one row
+#'
+#' @param        .x            Any R object
+#' @return       Boolean: is `.x` a non-empty data-frame
+#'
+#' @noRd
+
+is_nonempty_df <- function(.x) {
+  if (missing(.x)) {
+    stop(".x should be defined in is_nonempty_df")
+  }
+  is.data.frame(.x) &&
+    nrow(.x) > 0 &&
+    ncol(.x) > 0
+}
+
+###############################################################################
+
+#' - `fcounts_df` should have first two cols "Geneid" and "Length".
+#' - Both Length and all cols other than "Geneid" (of which there should be
+#' >= 1) should be numeric.
+#' - This only tests the structure of the data.frame; each count column
+#' should also correspond to a sample id in sample_df, this is tested later.
+#'
+#' @param        df            Some data-frame
+#'
+#' @noRd
+
+is_valid_fcounts_df <- function(df) {
+  is_nonempty_df(df) &&
+    ncol(df) >= 3 &&
+    all(colnames(df)[1:2] == c("Geneid", "Length")) &&
+    is.numeric(df[["Length"]]) &&
+    all(vapply(df[, -(1:2)], is.numeric, logical(1)))
 }
