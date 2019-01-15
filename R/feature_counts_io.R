66230c2f868885d54a0e5b9192e7a7edd6459962
Russell Hyde <russ_hyde@hotmail.com>
2019-01-15 14:31:58 +0000

    new (unimplemented) functions to read feature-counts files

---
+++ R/feature_counts_io.R
@@ -0,0 +1,28 @@
+###############################################################################
+
+# Functions for importing tables from featureCounts
+
+###############################################################################
+
+#' @export
+
+read_feature_counts <- function(
+                                files,
+                                sample_ids,
+                                comment = "#",
+                                col_types = "cii",
+                                progress = FALSE) {
+  NULL
+}
+
+###############################################################################
+
+read_single_feature_count_file <- function(
+                                           file,
+                                           comment = "#",
+                                           col_types = "cii",
+                                           progress = FALSE) {
+  NULL
+}
+
+###############################################################################
