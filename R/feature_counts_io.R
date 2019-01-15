6808252bf7bb515e278fbde31a2ee1b1b1a089d1
Russell Hyde <russ_hyde@hotmail.com>
2019-01-15 15:44:05 +0000

    add function to read from a single featureCounts file and reformat the header

--- R/feature_counts_io.R
+++ R/feature_counts_io.R
@@ -4,6 +4,18 @@
 
 ###############################################################################
 
+#' Import read counts from a set of featureCounts files and combine them into
+#' a single `data.frame`
+#'
+#' @param        files         A vector of file-paths. Each should be an output
+#'   from `featureCounts` and contain, at least, the `Geneid`, `Length` and
+#'   feature-count columns.
+#' @param        sample_ids    A vector of sample IDs corresponding, in order,
+#'   to the entries in `files`. If `NULL` ...
+#' @param        comment,col_types,progress   Arguments passed through to
+#'   `readr::read_tsv` that specify the format of the input files and whether
+#'   a progress bar should be shown.
+#'
 #' @export
 
 read_feature_counts <- function(
@@ -17,12 +29,33 @@ read_feature_counts <- function(
 
 ###############################################################################
 
-read_single_feature_count_file <- function(
-                                           file,
-                                           comment = "#",
-                                           col_types = "cii",
-                                           progress = FALSE) {
-  NULL
+#' Read a single featureCounts file
+#'
+#' @param        file          A single file, containing the `Geneid`, `Length`
+#'   and count columns from a single run of `featureCounts`.
+#' @inheritParams   read_feature_counts
+#'
+#' @importFrom   dplyr         rename_
+#' @importFrom   magrittr      %>%
+#' @importFrom   readr         read_tsv
+
+read_single_feature_counts_file <- function(
+                                            file,
+                                            comment = "#",
+                                            col_types = "cii",
+                                            progress = FALSE) {
+  fcounts <- readr::read_tsv(
+    file,
+    comment = comment, col_types = col_types, progress = progress
+  ) %>%
+    dplyr::rename_(
+      feature_id = ~Geneid,
+      length = ~Length
+    )
+
+  stopifnot(is_valid_fcounts_df(fcounts))
+
+  fcounts
 }
 
 ###############################################################################
--- tests/testthat/test_feature_counts_io.R
+++ tests/testthat/test_feature_counts_io.R
@@ -5,7 +5,33 @@ context("Tests for importing `featureCounts` datasets")
 ###############################################################################
 
 test_that("read_single_feature_counts_file", {
+  expect_equal(
+    read_single_feature_counts_file("feature_counts/short_1.tsv"),
+    tibble::tibble(
+      feature_id = c("ENSG00000223972", "ENSG00000227232"),
+      length = c(1735L, 1351L),
+      "some/file/name.bam" = c(0L, 2L)
+    ),
+    info = "read a single valid short-format featureCounts file"
+  )
 
+  m <- mockery::mock(
+    tibble::tibble(
+      Geneid = c("abc", "def", "ghi"),
+      Length = c(123L, 234L, 345L),
+      counts = c(1L, 0L, 10L),
+      non_numeric = c("shouldn't", "be", "possible")
+    )
+  )
+  with_mock(
+    read_tsv = m, {
+      expect_error(
+        read_single_feature_counts_file("malformed_contents_file"),
+        info = "All non-ID columns should be numeric"
+      )
+    },
+    .env = "readr"
+  )
 })
 
 ###############################################################################
