472af361bf166924d275aaece2a9ba141e35b62e
Russell Hyde <russ_hyde@hotmail.com>
2019-04-23 12:02:24 +0100

    new function `parse_colon_separated_lines`
    Splits each entry in a string vector into (field, value) pairs at the first colon. Returns the split values in a `data_frame` (flanking whitespace is removed from both field-name and value).

--- R/cutadapt_io.R
+++ R/cutadapt_io.R
@@ -1,3 +1,48 @@
+#' parse_colon_separated_lines
+#'
+#' Split each entry in a vector on the first colon. Strips flanking whitespace
+#' from what remains. Returns the left-hand-side in the column `field` and the
+#' right-hand-side in a column `value`.
+#'
+#' Not exported
+#'
+#' @param        x            Vector of colon-separated values.
+#'
+#' @return       `data_frame` with two columns: 'field' and 'value'. Any
+#'   leading/trailing whitespace is trimmed off both the field and value.
+#'
+#' @importFrom   dplyr         mutate_all
+#' @importFrom   magrittr      %>%   set_colnames
+#' @importFrom   stringr       str_split_fixed   str_replace
+#' @importFrom   tibble        as_data_frame
+#'
+
+parse_colon_separated_lines <- function(x) {
+  if (missing(x)) {
+    stop(
+      "character vector `x` should be defined in parse_colon_separated_lines"
+    )
+  }
+
+  # die if any of the input vector lacks a colon
+  stopifnot(all(grepl(":", x)))
+
+  strip_flanking_blanks <- function(x) {
+    x %>%
+      stringr::str_replace("^[[:blank:]]+", "") %>%
+      stringr::str_replace("[[:blank:]]+$", "")
+  }
+
+  x %>%
+    # Split on the first colon
+    stringr::str_split_fixed(":", n = 2) %>%
+    # Join the values into a two-column dataframe
+    magrittr::set_colnames(c("field", "value")) %>%
+    tibble::as_data_frame() %>%
+    # Strip all leading or trailing whitespace:
+    dplyr::mutate_all(strip_flanking_blanks)
+}
+
 #' Takes the 'summary' text from a cutadapt log-file and extracts the number of
 #' readpairs / basepairs that were input and which pass/fail various filters.
 #'
@@ -11,7 +56,7 @@
 parse_cutadapt_summary <- function(x) {
   # Should x be newline-stripped / bareline stripped /
   #   have the "=== SUMMARY ===" line removed prior to calling this?
-  if(missing(x)){
+  if (missing(x)) {
     stop("`x` should be defined in `parse_cutadapt_summary`")
   }
 
@@ -34,9 +79,9 @@ parse_cutadapt_summary <- function(x) {
 
   # parse_numeric_fields(x, fieldnames)
   tibble::data_frame(
-    rp_input = 114728,    r1_with_adapter = 2923, r2_with_adapter = 3833,
-    rp_too_short = 57,    rp_output = 114671,
-    bp_input = 17254556,  bp_input_r1 = 8628890,  bp_input_r2 = 8625666,
+    rp_input = 114728, r1_with_adapter = 2923, r2_with_adapter = 3833,
+    rp_too_short = 57, rp_output = 114671,
+    bp_input = 17254556, bp_input_r1 = 8628890, bp_input_r2 = 8625666,
     bp_output = 17217852, bp_output_r1 = 8612728, bp_output_r2 = 8605124
   )
 }
--- tests/testthat/test_cutadapt_io.R
+++ tests/testthat/test_cutadapt_io.R
@@ -8,8 +8,71 @@ context("Tests for cutadapt logfile importing in `reeq`")
 
 ###############################################################################
 
-test_that("cutadapt summary-text parser", {
+test_that("parse_colon_separated_lines", {
+  expect_error(
+    object = parse_colon_separated_lines(),
+    info = "`x` should be defined in parse_colon_separated_lines"
+  )
 
+  expect_error(
+    object = parse_colon_separated_lines("abc 123"),
+    info = "each entry of `x` should contain a colon"
+  )
+
+  expect_equal(
+    object = parse_colon_separated_lines(c()),
+    expected = tibble::data_frame(
+      field = character(0),
+      value = character(0)
+    ),
+    info = paste(
+      "parse_colon_separated_lines on an empty vector returns an empty,",
+      "2-column data_frame"
+    )
+  )
+
+  expect_equal(
+    object = parse_colon_separated_lines(
+      c("abc:123", "def:456", "ghi:some_value")
+    ),
+    expected = tibble::data_frame(
+      field = c("abc", "def", "ghi"),
+      value = c("123", "456", "some_value")
+    ),
+    info = "parse_colon_separated_lines without whitespace"
+  )
+
+  expect_equal(
+    object = parse_colon_separated_lines(
+      c(
+        "  abc:   123", "\t\tdef:456", "ghi:   some_value\t\t"
+      )
+    ),
+    expected = tibble::data_frame(
+      field = c("abc", "def", "ghi"),
+      value = c("123", "456", "some_value")
+    ),
+    info = paste(
+      "parse_colon_separated_lines strips trailing/leading whites"
+    )
+  )
+
+  expect_equal(
+    object = parse_colon_separated_lines(
+      c("spacey fieldname: 123", "field: spacey value")
+    ),
+    expected = tibble::data_frame(
+      field = c("spacey fieldname", "field"),
+      value = c("123", "spacey value")
+    ),
+    info = paste(
+      "parse_colon_separated_lines does not strip field/value-embedded",
+      "whitespace"
+    )
+  )
+})
+
+test_that("cutadapt summary-text parser", {
   expect_error(
     object = parse_cutadapt_summary(),
     info = "parse_cutadapt_summary should be provided some text as argument"
@@ -30,11 +93,11 @@ Total written (filtered):     17,217,852 bp (99.8%)
   Read 2:     8,605,124 bp"
 
   expect_equal(
-    object   = parse_cutadapt_summary(example),
+    object = parse_cutadapt_summary(example),
     expected = tibble::data_frame(
-      rp_input = 114728,    r1_with_adapter = 2923, r2_with_adapter = 3833,
-      rp_too_short = 57,    rp_output = 114671,
-      bp_input = 17254556,  bp_input_r1 = 8628890,  bp_input_r2 = 8625666,
+      rp_input = 114728, r1_with_adapter = 2923, r2_with_adapter = 3833,
+      rp_too_short = 57, rp_output = 114671,
+      bp_input = 17254556, bp_input_r1 = 8628890, bp_input_r2 = 8625666,
       bp_output = 17217852, bp_output_r1 = 8612728, bp_output_r2 = 8605124
     ),
     info = "Single cutadapt-summary parsing example"
