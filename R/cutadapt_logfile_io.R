ad40147e0e277825876d19994bf18889500dd409
Russell Hyde <russ_hyde@hotmail.com>
2019-04-25 17:14:12 +0100

    move logfile-parsing helper functions
    Renamed `cutadapt_io.R` as `cutadapt_logfile_io.R`
    
    - indicates that the functions relate to log-file parsing
    
    Extracted helper functions (that will be used in hisat2 logfile
    parsing) into a separated file `logfile_helpers.R`
    
    - ensured the latter is loaded before `cutadapt_logfile_io.R`

---
+++ R/cutadapt_logfile_io.R
@@ -0,0 +1,110 @@
+###############################################################################
+
+# ---- Main `cutadapt` parser
+
+#' Extract the basepair/readpair counts from the 'summary' section in the text
+#' from a cutadapt log-file.
+#'
+#' @param        x             A filename. This should be a cutadapt logfile.
+#'   The function dies if this is not an existing file or if more than one
+#'   entry is present in \code{x}.
+#'
+#' @importFrom   readr         read_file
+#' @export
+#'
+
+import_cutadapt_summary <- function(x) {
+  # x is a file path
+
+  # validate
+  stopifnot(length(x) == 1)
+  stopifnot(file.exists(file.path(x)))
+
+  # read
+  log_txt <- readr::read_file(x)
+
+  # parse the cutadapt logfile
+  summary_txt <- extract_cutadapt_summary(log_txt)
+
+  # convert the summary-data to a tibble and return
+  parse_cutadapt_summary(summary_txt)
+}
+
+#' Extract the 'summary' section from the text from a cutadapt log-file.
+#'
+#' Returns a single string.
+#'
+#' Not exported.
+#'
+#' @param        x             The raw text from a cutadapt logfile. This fails
+#'   if more than one text entry is present, so the user should not split the
+#'   file on newlines or anything like that before calling this function.
+#'
+
+extract_cutadapt_summary <- function(x) {
+  # `x` is the bare text from a cutadapt logfile
+
+  # return the bare text for the `summary` section of the logfile
+
+  stopifnot(is.character(x) && length(x) == 1)
+  # Extract the "Summary" section of the file
+  without_header <- gsub("^.*=== Summary ===\n", "", x)
+  without_footer <- gsub(
+    "\n\n=== First read: Adapter 1 ===.*$", "", without_header
+  )
+
+  without_footer
+}
+
+
+#' Takes the 'summary' text from a cutadapt log-file and extracts the number of
+#' readpairs / basepairs that were input and which pass/fail various filters.
+#'
+#' Not exported
+#'
+#' @param        x             Text from the summary section of a cutadapt log.
+#'
+#' @include      logfile_helpers.R
+#'
+
+parse_cutadapt_summary <- function(x) {
+  # `x` should be newline-stripped / bareline stripped /
+  #   have the "=== SUMMARY ===" line removed prior to calling this
+  if (missing(x)) {
+    stop("`x` should be defined in `parse_cutadapt_summary`")
+  }
+
+  fieldnames <- define_cutadapt_summary_renaming()
+
+  parse_numeric_fields(x, fieldnames)
+}
+
+###############################################################################
+
+# ---- Helpers
+
+###############################################################################
+
+#' Define the renaming of the fields present in a cutadapt log summary section
+#'
+#' @importFrom   tibble        tribble
+#'
+
+define_cutadapt_summary_renaming <- function() {
+  tibble::tribble(
+    ~expected, ~output,
+    "Total read pairs processed", "rp_input",
+    "Read 1 with adapter", "r1_with_adapter",
+    "Read 2 with adapter", "r2_with_adapter",
+    "Pairs that were too short", "rp_too_short",
+    "Pairs that were too long", "rp_too_long",
+    "Pairs with too many N", "rp_too_many_n",
+    "Pairs written (passing filters)", "rp_output",
+    "Total basepairs processed", "bp_input",
+    "Read 1", "bp_input_r1",
+    "Read 2", "bp_input_r2",
+    "Total written (filtered)", "bp_output",
+    "Read 1", "bp_output_r1",
+    "Read 2", "bp_output_r2"
+  )
+}
--- tests/testthat/test_logfile_helpers.R
+++ tests/testthat/test_logfile_helpers.R
@@ -1,14 +1,8 @@
 ###############################################################################
 
-context("Tests for cutadapt logfile importing in `reeq`")
+context("Tests for logfile-helper functions")
 
 ###############################################################################
-
-# format conversion tests
-
-###############################################################################
-
-#
 
 test_that("parse_colon_separated_lines", {
   expect_error(
@@ -74,9 +68,9 @@ test_that("parse_colon_separated_lines", {
   )
 })
 
-#
+###############################################################################
 
-test_that("parse_numeric_fields from a cutadapt-log text", {
+test_that("parse_numeric_fields from a colon-separated logfile text", {
   expect_error(
     object = parse_numeric_fields(),
     info = paste(
@@ -131,83 +125,4 @@ test_that("parse_numeric_fields from a cutadapt-log text", {
     )
   )
 })
-
-###############################################################################
-
-# cutadapt parser tests
-
-###############################################################################
-
-test_that("cutadapt summary extractor", {
-  prefix <- "This is cutadapt 1.13 with Python 3.6.7
-Blah blah blah
-
-=== Summary ===
-"
-
-  summary_section <- "Total read pairs processed:         14,605,217
-  Read 1 with adapter:                 482,313 (3.3%)
-  Read 2 with adapter:               1,012,028 (6.9%)
-Pairs that were too short:               8,529 (0.1%)
-Pairs with too many N:                   3,241 (0.0%)
-Pairs written (passing filters):    14,593,447 (99.9%)
-
-Total basepairs processed: 2,205,292,673 bp
-  Read 1: 1,103,006,677 bp
-  Read 2: 1,102,285,996 bp
-Total written (filtered):  2,191,574,660 bp (99.4%)
-  Read 1: 1,093,901,386 bp
-  Read 2: 1,097,673,274 bp"
-
-  suffix <- "
-
-=== First read: Adapter 1 ===
-
-Sequence: AAAAAAAAAAAAA..; Type: regular 3'; Length: 76; Trimmed: 173335 times.
-
-Blah blah blah
-"
-
-  text <- paste0(prefix, summary_section, suffix, sep = "\n")
-
-  expect_equal(
-    extract_cutadapt_summary(text),
-    summary_section,
-    info = "Extract the summary section from the text in a cutadapt log"
-  )
-})
-
-
-###############################################################################
-
-test_that("cutadapt summary-text parser", {
-  expect_error(
-    object = parse_cutadapt_summary(),
-    info = "parse_cutadapt_summary should be provided some text as argument"
-  )
-
-  example <-
-    "Total read pairs processed:            114,728
-  Read 1 with adapter:                   2,923 (2.5%)
-  Read 2 with adapter:                   3,833 (3.3%)
-Pairs that were too short:                  57 (0.0%)
-
-Pairs written (passing filters):       114,671 (100.0%)
-Total basepairs processed:    17,254,556 bp
-  Read 1:     8,628,890 bp
-  Read 2:     8,625,666 bp
-Total written (filtered):     17,217,852 bp (99.8%)
-  Read 1:     8,612,728 bp
-  Read 2:     8,605,124 bp"
-
-  expect_equal(
-    object = parse_cutadapt_summary(example),
-    expected = tibble::tibble(
-      rp_input = 114728, r1_with_adapter = 2923, r2_with_adapter = 3833,
-      rp_too_short = 57, rp_output = 114671,
-      bp_input = 17254556, bp_input_r1 = 8628890, bp_input_r2 = 8625666,
-      bp_output = 17217852, bp_output_r1 = 8612728, bp_output_r2 = 8605124
-    ),
-    info = "Single cutadapt-summary parsing example"
-  )
-})
+##############################################################################
