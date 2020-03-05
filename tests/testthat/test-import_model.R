5f47ea32501833a1f0be89daba0d475e69d432b0
Russell Hyde <russ.hyde.data@gmail.com>
2020-03-05 14:54:18 +0000

    tests for import_design()

---
+++ tests/testthat/test-import_model.R
@@ -0,0 +1,85 @@
+###############################################################################
+
+context(
+  paste(
+    "model design, contrasts, contrast-collection, lrt-tree and",
+    "plot-comparisons can be imported"
+  )
+)
+
+###############################################################################
+
+test_that("a design matrix can be imported from .tsv", {
+  # .tsv contains:
+  # - any number >= 2 of columns
+  # - a sample ID column with values matching the sample IDs in the DGEList
+
+  # Constraints
+  # - .tsv file must exist
+  # - DGEList may be provided, and it is:
+  #   - it should have sample IDs
+  #   - rownames of the design should match the samples in the DGEList
+
+  # happy path:
+  # - sample IDs ("s1", "s2") are present in the DGEList
+  # - sample IDs occur in the same order in 1st column of `design.tsv`
+
+  # TODO:
+  # - non-numeric entries in the design file -> error
+  # - a design file that causes readr::read_tsv to fail
+
+  design_file <- file.path("model_tables", "design.tsv")
+
+  my_dge <- get_dge1(add_genes = TRUE)
+  expected_design <- matrix(
+    c(1, 1, 0, 1),
+    nrow = 2,
+    dimnames = list(c("s1", "s2"), c("intercept", "f2"))
+  )
+
+  expect_equivalent(
+    import_design(design_file, dge = my_dge),
+    expected_design,
+    info = "import a design from a file & validate against a DGEList"
+  )
+
+  expect_equivalent(
+    import_design(design_file, dge = my_dge[, 2:1]),
+    expected_design[2:1, ],
+    info = paste(
+      "samples in the design will be reordered to match that in the DGEList"
+    )
+  )
+
+  expect_equivalent(
+    import_design(file.path("model_tables", "design.tsv")),
+    expected_design,
+    info = "when no DGEList is provided, just import the design matrix"
+  )
+
+  expect_error(
+    import_design(
+      file.path("model_tables", "design.tsv"),
+      dge = magrittr::set_colnames(dge, c("X", "Y"))
+    ),
+    info = "sample IDs of imported design should match DGEList"
+  )
+
+  expect_error(
+    import_design(file.path("NOT-A-FILE"), dge = my_dge),
+    info = "design .tsv should exist"
+  )
+
+  # sample names are correct, but class should be DGEList
+  df <- data.frame(s1 = 1, s2 = 2)
+  expect_error(
+    import_design(file.path("model_tables", "design.tsv"), dge = df),
+    info = "if `dge` is provided, it should be a DGEList"
+  )
+})
+
+###############################################################################
+
+test_that("a contrast matrix can be imported from a .tsv", {
+
+})
